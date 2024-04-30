c13c5a915577788a67a7afd216b14ef44663851a
mereluo <meredithluo710@gmail.com>
2024-04-30 11:36:15 -0700

    Caching for performance gains

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from "esbuild-wasm";
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
+import { fetchPlugin } from "./plugins/fetch-plugins";
 
 const App = () => {
     const ref = useRef<any>();
@@ -11,7 +12,7 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: "/esbuild.wasm",
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
         });
     };
 
@@ -23,7 +24,16 @@ const App = () => {
         if (!ref.current) {
             return;
         }
-        const result = await ref.current.build({ entryPoints: ["index.js"], bundle: true, write: false, plugins: [unpkgPathPlugin()] });
+        const result = await ref.current.build({
+            entryPoints: ["index.js"],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+            define: {
+                "process.env.NODE_ENV": '"production"',
+                global: "window",
+            },
+        });
         // console.log(result);
         setCode(result.outputFiles[0].text);
     };
